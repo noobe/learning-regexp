c7382c86f5ba4cb105a538b223542a5e1977c1b5
Sarath <sarath.regedit@gmail.com>
2022-11-16 22:12:57 +0530

    G Flag

--- 02 - Flags/README.md
+++ 02 - Flags/README.md
@@ -6,3 +6,20 @@ Some commonly used ones are:
 - i: case-insensitive search
 - m: multi line search
 - u: unicode search
+
+## g: Global search
+```js
+const regex1 = /the/g;
+str = 'Please get The apple, the ball and the bat';
+
+console.log(str.match(regex1));
+console.log(str.search(regex1));
+console.log(str.replace(regex1, "TEST"));
+```
+gives:
+```js
+[ 'the', 'the' ]
+22
+Please get The apple, TEST ball and TEST bat
+```
+Here the string contain 2 occurance of the word 'the' and setting the flag g results in finding/replacing both the occurances. Search returns the index of the 1st occurance of the word.
--- 02 - Flags/app.js
+++ 02 - Flags/app.js
@@ -1,19 +1,19 @@
 const regex1 = /the/g;
-str = 'Please get The apple and the ball';
+str = 'Please get The apple, the ball and the bat';
 
 console.log(str.match(regex1));
 console.log(str.search(regex1));
 console.log(str.replace(regex1, "TEST"));
 
 const regex2 = /the/i;
-str = 'Please get the apple and the ball';
+str = 'Please get The apple, the ball and the bat';
 
 console.log(str.match(regex2));
 console.log(str.search(regex2));
 console.log(str.replace(regex2, "TEST"));
 
 const regex3 = /the/gi;
-str = 'Please get the apple and the ball';
+str = 'Please get The apple, the ball and the bat';
 
 console.log(str.match(regex3));
 console.log(str.search(regex3));
